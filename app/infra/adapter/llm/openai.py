5a4796d672c08a5cc1c2100b4ad41e4f7981a836
Gustavo Ortega <gustavo_ortega@usp.br>
2026-01-07 23:38:37 -0300

    feat: add bind_tools and structured_output - openai class

--- app/infra/adapter/llm/openai.py
+++ app/infra/adapter/llm/openai.py
@@ -2,7 +2,17 @@ from app.interfaces.llm.interface import LLMInterface
 from app.interfaces.llm.models import LLMResponse
 from app.settings.config import settings
 
+from langchain_core.runnables.base import Runnable
+from langchain_core.language_models import LanguageModelInput
+from langchain_core.tools import BaseTool
+from langchain_core.messages import AIMessage
 from langchain_openai import ChatOpenAI
+from langchain_openai.chat_models.base import (
+    _DictOrPydantic,
+    _DictOrPydanticClass
+)
+from typing import Any, Literal
+from collections.abc import Callable, Sequence
 
 
 class AdapterOpenAI(LLMInterface):
@@ -15,10 +25,7 @@ class AdapterOpenAI(LLMInterface):
         self.__client = ChatOpenAI(
             model=settings.LLM_MODEL,
             temperature=settings.LLM_TEMPERATURE,
-            openai_api_key=settings.OPENAI_API_KEY,
-            model_kwargs={
-                "validate_openai_api_key": True
-            }
+            openai_api_key=settings.OPENAI_API_KEY
         )
 
     async def ainvoke(self, input, config=None, *, stop=None, **kwargs):
@@ -34,6 +41,44 @@ class AdapterOpenAI(LLMInterface):
             input,
             config=config,
             stop=stop,
+            **kwargs,
+        )
+
+    def bind_tools(
+        self,
+        tools: Sequence[dict[str, Any] | type | Callable | BaseTool],
+        *,
+        tool_choice: dict | str | bool | None = None,
+        strict: bool | None = None,
+        parallel_tool_calls: bool | None = None,
+        response_format: _DictOrPydanticClass | None = None,
+        **kwargs: Any,
+    ) -> Runnable[LanguageModelInput, AIMessage]:
+        return self.__client.bind_tools(
+            tools,
+            tool_choice=tool_choice,
+            strict=strict,
+            parallel_tool_calls=parallel_tool_calls,
+            response_format=response_format,
+            **kwargs,
+        )
+
+    def with_structured_output(
+        self,
+        schema: _DictOrPydanticClass | None = None,
+        *,
+        method: Literal["function_calling", "json_mode", "json_schema"] = "json_schema",
+        include_raw: bool = False,
+        strict: bool | None = None,
+        tools: list | None = None,
+        **kwargs: Any,
+    ) -> Runnable[LanguageModelInput, _DictOrPydantic]:
+        return self.__client.with_structured_output(
+            schema,
+            method=method,
+            include_raw=include_raw,
+            strict=strict,
+            tools=tools,
             **kwargs,
         )
 
